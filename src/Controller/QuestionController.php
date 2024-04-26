7229f36784caf7c2590c4ec75ceb346cf06046b8
QDZantoine <antoine.quendez@gmail.com>
2024-04-26 11:37:50 +0200

    Update the project with dyma courses

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,26 +2,60 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Entity\Question;
+use App\Form\CommentType;
 use App\Form\QuestionType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 
 class QuestionController extends AbstractController
 {
     #[Route('/question/ask', name: 'question_form')]
-    public function index(Request $request): Response
-    {
-        $formQuestion = $this->createForm(QuestionType::class);
+    public function index(Request $request,EntityManagerInterface $em): Response
+    {   $question = new Question();
+        $formQuestion = $this->createForm(QuestionType::class,$question);
         $formQuestion->handleRequest($request);
-        if($formQuestion->isSubmitted() && $formQuestion->isValid()){
-            dump($formQuestion->getData());
+        if ($formQuestion->isSubmitted() && $formQuestion->isValid()) {
+            $question->setNbrOfResponse(0);
+            $question->setRating(0);
+            $question->setCreatedAt(new \DateTimeImmutable());
+            $em->persist($question);
+            $em->flush();
+            $this->addFlash('success', 'Votre Question à été ajouté !');
+            return $this->redirectToRoute('app_home');
+
+            
         }
 
 
         return $this->render('question/index.html.twig', [
             'form' => $formQuestion->createView(),
+        ]);
+    }
+    #[Route('/question/{id}', name: 'question_show')]
+    /** @var Question */
+    public function show(Request $request,Question $question,EntityManagerInterface $em): Response
+    {
+        $comment= new Comment();
+        $commentForm = $this->createForm(CommentType::class,$comment);
+        $commentForm->handleRequest($request);
+        if($commentForm->isSubmitted() && $commentForm->isValid()){
+            $comment->setCreatedAt(new \DateTimeImmutable());
+            $comment->setRating(0);
+            $comment->setQuestion($question);
+            $em->persist($comment);
+            $em->flush();
+            $this->addFlash('success', 'Votre Commentaire à été ajouté');
+            return $this->redirect($request->getUri());
+        }
+        
+        return $this->render('question/show.html.twig', [
+            'question' => $question,
+            'form'=> $commentForm->createView()
         ]);
     }
 }
--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -2,27 +2,26 @@
 
 namespace App\Form;
 
+use App\Entity\Comment;
 use App\Entity\Question;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
-class QuestionType extends AbstractType
+class CommentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title',TextType::class)
-            ->add('content',TextareaType::class);
+            ->add('content',TextareaType::class, ['label' => false]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            // Configure your form options here
-            'data_class' => Question::class,
+            'data_class' => Comment::class,
         ]);
     }
 }
